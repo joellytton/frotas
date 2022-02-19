75ecc1c2a9e236d5af402bc27400ad59e96aed6b
joellytton <joellytton25@gmail.com>
2022-02-19 10:14:18 -0500

    iniciando o cadastro de categoria veiculo

--- resources/views/administracao/categoria_veiculo/create.blade.php
+++ resources/views/administracao/categoria_veiculo/create.blade.php
@@ -17,4 +17,30 @@
             </div>
         </div><!-- /.container-fluid -->
     </section>
+
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col">
+                    <div class="card card-primary">
+                        <!-- form start -->
+                        <form action="{{route('')}}" method="post">
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <label for="nome">Nome</label>
+                                    <input type="text" class="form-control" id="nome" placeholder="Nome">
+                                </div>
+                            </div>
+                            <!-- /.card-body -->
+
+                            <div class="card-footer">
+                                <button type="submit" class="btn btn-primary">Salvar</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </section>
 @endsection
