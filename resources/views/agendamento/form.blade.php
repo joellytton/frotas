552fc0cfacaaeec424f570863583a5941ba45f27
joellytton <joellytton25@gmail.com>
2022-02-23 00:31:51 -0500

    teste na tela de agendamento

--- resources/views/agendamento/form.blade.php
+++ resources/views/agendamento/form.blade.php
@@ -1,81 +1,165 @@
 <!-- form start -->
+<div class="card-header">
+    <h3 class="card-title">
+        Informações do Solicitante
+    </h3>
+</div>
 <div class="card-body">
     <div class="form-group row">
-        <div class="col-4">
-            <label for="nome">Nome</label>
+        <div class="col-6">
+            <label for="nome">Nome:</label> JOELLYTTON MENDONÇA DOS SANTOS
+            <br>
+            <label for="nome">CPF:</label> 020.748.252-77
+            <br>
+            <label for="nome">RG:</label> 9.878.878-8
+            <br>
+            <label for="nome">Contato:</label> (11) 99876-5432
+        </div>
 
-            <input type="text" class="form-control {{$errors->has('nome') ? 'is-invalid' : ''}}"
-                   name="nome" id="nome" placeholder="Nome" aria-describedby="nomeFeedback"
-                   value="{{old('nome', @$motorista->nome)}}" maxlength="250">
-            @if ($errors->has('nome'))
-                <div id="nomeFeedback" class="invalid-feedback">
-                    {{$errors->first('nome')}}
+        <div class="col-6">
+            <label for="cpf">Email:</label> joellytton25@gmail.com
+            <br>
+            <label for="cpf">Departamento:</label> Departamento de Informática
+            <br>
+            <label for="cpf">Função:</label> Analista de Sistemas
+        </div>
+    </div>
+</div>
+<!-- /.card-body solicitante  -->
+<div class="card-header">
+    <h3 class="card-title">
+        Informações de Deslocamento
+    </h3>
+</div>
+
+<div class="card-body">
+    <div class="form-group row">
+        <div class="col-2 d-inline-flex align-items-end">
+            <label for="nome">Local de origem: </label>
+        </div>
+
+        <div class="col-4 d-inline-flex">
+            <select name="origem" class="form-control {{$errors->has('origem') ? 'is-invalid' : ''}}"
+                    aria-describedby="origemFeedback">
+                <option value="">Selecione</option>
+                <option value="1">São Paulo</option>
+                <option value="2">Rio de Janeiro</option>
+                <option value="3">Belo Horizonte</option>
+                <option value="4">Curitiba</option>
+            </select>
+
+            @if ($errors->has('origem'))
+                <div id="origemFeedback" class="invalid-feedback">
+                    {{$errors->first('origem')}}
                 </div>
             @endif
         </div>
 
-        <div class="col-4">
-            <label for="cpf">CPF</label>
-
-            <input type="text" class="form-control {{$errors->has('cpf') ? 'is-invalid' : ''}}"
-                   name="cpf" id="cpf" placeholder="CPF" aria-describedby="cpfFeedback"
-                   data-inputmask="'mask': ['999.999.999-99']" data-mask
-                   value="{{old('cpf', @$motorista->cpf)}}" maxlength="14">
-            @if ($errors->has('cpf'))
-                <div id="cpfFeedback" class="invalid-feedback">
-                    {{$errors->first('cpf')}}
-                </div>
-            @endif
-        </div>
-
-        <div class="col-4">
-            <label for="data_nascimento">Data de Nascimento</label>
-
-            <input type="text" class="form-control {{$errors->has('data_nascimento') ? 'is-invalid' : ''}}"
-                   name="data_nascimento" id="data_nascimento" placeholder="Data de Nascimento"
-                   aria-describedby="dataNascimentoFeedback"
-                   value="{{old('data_nascimento', @$motorista->data_nascimento)}}"
-                   data-inputmask="'mask': ['99/99/9999']" data-mask>
-            @if ($errors->has('data_nascimento'))
-                <div id="dataNascimentoFeedback" class="invalid-feedback">
-                    {{$errors->first('data_nascimento')}}
+        <div class="col-6 d-inline-flex">
+            <input type="text" class="form-control {{$errors->has('local_origem') ? 'is-invalid' : ''}}"
+                   name="nome" id="nome" placeholder="Informe o local de origem aqui..."
+                   aria-describedby="local_origemFeedback"
+                   value="{{old('local_origem', @$motorista->nome)}}" maxlength="250">
+            @if ($errors->has('local_origem'))
+                <div id="local_origemFeedback" class="invalid-feedback">
+                    {{$errors->first('local_origem')}}
                 </div>
             @endif
         </div>
     </div>
 
     <div class="form-group row">
-        <div class="col-4">
-            <label for="numero_registro">Numero Registro</label>
+        <div class="col-2 d-inline-flex align-items-end">
+            <label for="destino">Local de destino: </label>
+        </div>
 
-            <input type="text" class="form-control {{$errors->has('numero_registro') ? 'is-invalid' : ''}}"
-                   name="numero_registro" id="numero_registro" placeholder="Numero Registro"
-                   aria-describedby="numero_registroFeedback"
-                   value="{{old('numero_registro', @$motorista->numero_registro)}}"
-                   maxlength="11">
-            @if ($errors->has('numero_registro'))
-                <div id="numero_registroFeedback" class="invalid-feedback">
-                    {{$errors->first('numero_registro')}}
+        <div class="col-4 d-inline-flex">
+            <select name="destino" class="form-control {{$errors->has('destino') ? 'is-invalid' : ''}}"
+                    aria-describedby="destinoFeedback">
+                <option value="">Selecione</option>
+                <option value="1">São Paulo</option>
+                <option value="2">Rio de Janeiro</option>
+                <option value="3">Belo Horizonte</option>
+                <option value="4">Curitiba</option>
+            </select>
+
+            @if ($errors->has('destino'))
+                <div id="destinoFeedback" class="invalid-feedback">
+                    {{$errors->first('destino')}}
                 </div>
             @endif
         </div>
 
-        <div class="col-4">
-            <label for="validade">Validade</label>
-
-            <input type="text" class="form-control {{$errors->has('validade') ? 'is-invalid' : ''}}"
-                   name="validade" id="validade" placeholder="Validade"
-                   aria-describedby="validadeFeedback" value="{{old('validade', @$motorista->validade)}}"
-                   data-inputmask="'mask': ['99/99/9999']" data-mask
-            >
-            @if ($errors->has('validade'))
-                <div id="validadeFeedback" class="invalid-feedback">
-                    {{$errors->first('validade')}}
+        <div class="col-6 d-inline-flex">
+            <input type="text" class="form-control {{$errors->has('local_origem') ? 'is-invalid' : ''}}"
+                   name="nome" id="nome" placeholder="Informe o local de destino aqui..."
+                   aria-describedby="local_origemFeedback"
+                   value="{{old('local_origem', @$motorista->nome)}}" maxlength="250">
+            @if ($errors->has('local_origem'))
+                <div id="local_origemFeedback" class="invalid-feedback">
+                    {{$errors->first('local_origem')}}
                 </div>
             @endif
         </div>
     </div>
 
+    <div class="form-group row">
+        <div class="col-2 d-inline-flex align-items-end">
+            <label for="destino">Tipo Veiculo: </label>
+        </div>
+
+        <div class="col-4 d-inline-flex">
+            <select name="destino" class="form-control {{$errors->has('destino') ? 'is-invalid' : ''}}"
+                    aria-describedby="destinoFeedback">
+                <option value="">Escolha o tipo de veiculo</option>
+                <option value="1">São Paulo</option>
+                <option value="2">Rio de Janeiro</option>
+                <option value="3">Belo Horizonte</option>
+                <option value="4">Curitiba</option>
+            </select>
+
+            @if ($errors->has('destino'))
+                <div id="destinoFeedback" class="invalid-feedback">
+                    {{$errors->first('destino')}}
+                </div>
+            @endif
+        </div>
+    </div>
+
+    <div class="form-group row">
+        <div class="col-2 d-inline-flex align-items-end">
+            <label for="nome">Data de Saida: </label>
+        </div>
+
+        <div class="col-4 d-inline-flex">
+            <input type="text" class="form-control {{$errors->has('local_origem') ? 'is-invalid' : ''}}"
+                   name="nome" id="nome" aria-describedby="local_origemFeedback"
+                   value="{{old('local_origem', @$motorista->nome)}}" maxlength="250">
+            @if ($errors->has('local_origem'))
+                <div id="local_origemFeedback" class="invalid-feedback">
+                    {{$errors->first('local_origem')}}
+                </div>
+            @endif
+        </div>
+    </div>
+
+    <div class="form-group row">
+        <div class="col-2 d-inline-flex align-items-end">
+            <label for="nome">Data de Retorno: </label>
+        </div>
+
+        <div class="col-4 d-inline-flex">
+            <input type="text" class="form-control {{$errors->has('local_origem') ? 'is-invalid' : ''}}"
+                   name="nome" id="nome" aria-describedby="local_origemFeedback"
+                   value="{{old('local_origem', @$motorista->nome)}}" maxlength="250">
+            @if ($errors->has('local_origem'))
+                <div id="local_origemFeedback" class="invalid-feedback">
+                    {{$errors->first('local_origem')}}
+                </div>
+            @endif
+        </div>
+
+    </div>
 </div>
 
 <div class="card-footer">
