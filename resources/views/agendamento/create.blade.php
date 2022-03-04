39a58c585f9f269336f5f929983b372786cf304c
joellytton <joellytton@pop-os.localdomain>
2022-03-04 16:58:56 -0500

    ajuste no agendamento

--- resources/views/agendamento/create.blade.php
+++ resources/views/agendamento/create.blade.php
@@ -24,12 +24,38 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col">
-                    <div class="card card-primary">
-                        <form action="{{route('agendamento.store')}}" method="post">
-                            @csrf
-                            @include('agendamento.form')
-                        </form>
+                    <div class="card-header">
+                        <h3 class="card-title">
+                            Informações do Solicitante
+                        </h3>
                     </div>
+
+                    <div class="card-body">
+                        <div class="form-group row">
+                            <div class="col-6">
+                                <label for="nome">Nome:</label> JOELLYTTON MENDONÇA DOS SANTOS
+                                <br>
+                                <label for="nome">CPF:</label> 020.748.252-77
+                                <br>
+                                <label for="nome">RG:</label> 9.878.878-8
+                                <br>
+                                <label for="nome">Contato:</label> (11) 99876-5432
+                            </div>
+
+                            <div class="col-6">
+                                <label for="cpf">Email:</label> joellytton25@gmail.com
+                                <br>
+                                <label for="cpf">Departamento:</label> Departamento de Informática
+                                <br>
+                                <label for="cpf">Função:</label> Analista de Sistemas
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.card-body solicitante  -->
+                    <form action="{{route('agendamento.store')}}" method="post" class="form-horizontal">
+                        @csrf
+                        @include('agendamento.form')
+                    </form>
                 </div>
             </div>
             <!-- /.row -->
--- resources/views/agendamento/form.blade.php
+++ resources/views/agendamento/form.blade.php
@@ -1,30 +1,3 @@
-<!-- form start -->
-<div class="card-header">
-    <h3 class="card-title">
-        Informações do Solicitante
-    </h3>
-</div>
-<div class="card-body">
-    <div class="form-group row">
-        <div class="col-6">
-            <label for="nome">Nome:</label> JOELLYTTON MENDONÇA DOS SANTOS
-            <br>
-            <label for="nome">CPF:</label> 020.748.252-77
-            <br>
-            <label for="nome">RG:</label> 9.878.878-8
-            <br>
-            <label for="nome">Contato:</label> (11) 99876-5432
-        </div>
-
-        <div class="col-6">
-            <label for="cpf">Email:</label> joellytton25@gmail.com
-            <br>
-            <label for="cpf">Departamento:</label> Departamento de Informática
-            <br>
-            <label for="cpf">Função:</label> Analista de Sistemas
-        </div>
-    </div>
-</div>
 <!-- /.card-body solicitante  -->
 <div class="card-header">
     <h3 class="card-title">
@@ -34,11 +7,8 @@
 
 <div class="card-body">
     <div class="form-group row">
-        <div class="col-2 d-inline-flex align-items-end">
-            <label for="nome">Local de origem: </label>
-        </div>
-
-        <div class="col-4 d-inline-flex">
+        <label for="origem" class="col-sm-12 col-md-2 col-form-label">Local de origem:</label>
+        <div class="col-sm-12 col-md-5">
             <select name="origem" class="form-control {{$errors->has('origem') ? 'is-invalid' : ''}}"
                     aria-describedby="origemFeedback">
                 <option value="">Selecione</option>
@@ -55,7 +25,7 @@
             @endif
         </div>
 
-        <div class="col-6 d-inline-flex">
+        <div class="col-sm-12 col-md-5">
             <input type="text" class="form-control {{$errors->has('local_origem') ? 'is-invalid' : ''}}"
                    name="nome" id="nome" placeholder="Informe o local de origem aqui..."
                    aria-describedby="local_origemFeedback"
@@ -69,13 +39,10 @@
     </div>
 
     <div class="form-group row">
-        <div class="col-2 d-inline-flex align-items-end">
-            <label for="destino">Local de destino: </label>
-        </div>
-
-        <div class="col-4 d-inline-flex">
-            <select name="destino" class="form-control {{$errors->has('destino') ? 'is-invalid' : ''}}"
-                    aria-describedby="destinoFeedback">
+        <label for="origem" class="col-sm-12 col-md-2 col-form-label">Local de destino</label>
+        <div class="col-sm-12 col-md-5">
+            <select name="origem" class="form-control {{$errors->has('origem') ? 'is-invalid' : ''}}"
+                    aria-describedby="origemFeedback">
                 <option value="">Selecione</option>
                 <option value="1">São Paulo</option>
                 <option value="2">Rio de Janeiro</option>
@@ -83,16 +50,16 @@
                 <option value="4">Curitiba</option>
             </select>
 
-            @if ($errors->has('destino'))
-                <div id="destinoFeedback" class="invalid-feedback">
-                    {{$errors->first('destino')}}
+            @if ($errors->has('origem'))
+                <div id="origemFeedback" class="invalid-feedback">
+                    {{$errors->first('origem')}}
                 </div>
             @endif
         </div>
 
-        <div class="col-6 d-inline-flex">
+        <div class="col-sm-12 col-md-5">
             <input type="text" class="form-control {{$errors->has('local_origem') ? 'is-invalid' : ''}}"
-                   name="nome" id="nome" placeholder="Informe o local de destino aqui..."
+                   name="nome" id="nome" placeholder="Informe o local de origem aqui..."
                    aria-describedby="local_origemFeedback"
                    value="{{old('local_origem', @$motorista->nome)}}" maxlength="250">
             @if ($errors->has('local_origem'))
@@ -127,28 +94,8 @@
     </div>
 
     <div class="form-group row">
-        <div class="col-2 d-inline-flex align-items-end">
-            <label for="nome">Data de Saida: </label>
-        </div>
-
-        <div class="col-4 d-inline-flex">
-            <input type="text" class="form-control {{$errors->has('local_origem') ? 'is-invalid' : ''}}"
-                   name="nome" id="nome" aria-describedby="local_origemFeedback"
-                   value="{{old('local_origem', @$motorista->nome)}}" maxlength="250">
-            @if ($errors->has('local_origem'))
-                <div id="local_origemFeedback" class="invalid-feedback">
-                    {{$errors->first('local_origem')}}
-                </div>
-            @endif
-        </div>
-    </div>
-
-    <div class="form-group row">
-        <div class="col-2 d-inline-flex align-items-end">
-            <label for="nome">Data de Retorno: </label>
-        </div>
-
-        <div class="col-4 d-inline-flex">
+        <label for="origem" class="col-sm-12 col-md-2 col-form-label">Data de Saida:</label>
+        <div class="col-sm-12 col-md-5">
             <input type="text" class="form-control {{$errors->has('local_origem') ? 'is-invalid' : ''}}"
                    name="nome" id="nome" aria-describedby="local_origemFeedback"
                    value="{{old('local_origem', @$motorista->nome)}}" maxlength="250">
@@ -159,6 +106,18 @@
             @endif
         </div>
 
+        <label for="origem" class="col-sm-12 col-md-1 col-form-label">Data de Retorno:</label>
+        <div class="col-sm-12 col-md-4">
+            <input type="text" class="form-control {{$errors->has('local_origem') ? 'is-invalid' : ''}}"
+                   name="nome" id="nome" placeholder="Previsão"
+                   aria-describedby="local_origemFeedback"
+                   value="{{old('local_origem', @$motorista->nome)}}" maxlength="250">
+            @if ($errors->has('local_origem'))
+                <div id="local_origemFeedback" class="invalid-feedback">
+                    {{$errors->first('local_origem')}}
+                </div>
+            @endif
+        </div>
     </div>
 </div>
 
